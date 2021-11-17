806526cdde10897b297af7402a8234ffcfaae04d
Rodrigo <rodrigokaconde@yahoo.com.br>
2021-11-16 22:21:19 -0300

    git initial configure pom

---
+++ src/test/java/com/rfoliveira/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfoliveira.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
