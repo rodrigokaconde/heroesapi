2496059bac8b61d5766eebd61dcda9508960f263
Rodrigo <rodrigokaconde@yahoo.com.br>
2021-11-17 22:33:52 -0300

    create config HeroesData, HeroesTable and constants

---
+++ src/main/java/com/rfoliveira/heroesapi/config/HeroesData.java
@@ -0,0 +1,34 @@
+package com.rfoliveira.heroesapi.config;
+
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+
+import static com.rfoliveira.heroesapi.constans.HeroesConstant.ENDPOINT_DYNAMO;
+import static com.rfoliveira.heroesapi.constans.HeroesConstant.REGION_DYNAMO;
+
+public class HeroesData {
+    public static void main(String[] args) throws Exception{
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(ENDPOINT_DYNAMO, REGION_DYNAMO))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("Heroes_Table");
+
+        Item hero = new Item()
+                .withPrimaryKey("id", 1)
+                .withString("name", "Mulher Maravilha")
+                .withString("universe", "Dc comics")
+                .withNumber("films",3);
+
+        PutItemOutcome outcome = table.putItem(hero);
+
+    }
+}
---
+++ src/main/java/com/rfoliveira/heroesapi/constans/HeroesConstant.java
@@ -0,0 +1,7 @@
+package com.rfoliveira.heroesapi.constans;
+
+public class HeroesConstant {
+    public static final String HEROES_ENDPOINT_LOCAL = "/heroes";
+    public static final String ENDPOINT_DYNAMO = "https://localhost:8000";
+    public static final String REGION_DYNAMO = "us-east-1";
+}
